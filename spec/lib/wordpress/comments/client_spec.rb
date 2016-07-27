e80cf389ba270d7301ec7349cf753b44a386b75b
Alex <alexiseliudrdz@gmail.com>
2016-07-27 16:18:38 -0500

    Using Context with specs

--- spec/lib/wordpress/comments/client_spec.rb
+++ spec/lib/wordpress/comments/client_spec.rb
@@ -4,6 +4,7 @@ require_relative '../../../support/match_date'
 
 describe Wordpress::Comments::Client do 
 	let(:client) {Wordpress::Comments::Client.new 'spec/fixtures/feed.xml'  } 
+	let(:xml) { File.read(File.join('spec','fixtures', 'feed.xml')) }
 
 	describe '#initialize' do
 		it "stores a URL" do
@@ -12,7 +13,6 @@ describe Wordpress::Comments::Client do
 		end
 
 	describe '#parse' do
-		let(:xml) { File.read(File.join('spec','fixtures', 'feed.xml')) }
 		let(:comments) { client.parse xml }
 		let(:comment) { comments.first } 
 		let(:link) {  'http://mashable.com/2012/07/18/ipad-early-photos/comment-page-1/#comment-18239503'}  
@@ -45,9 +45,43 @@ describe Wordpress::Comments::Client do
 
   describe '#fetch' do
     let(:comments) { client.fetch }
+	context "success" do
 
- 	it "builds comment objects" do
-    	expect(comments.length).to eq 30
-	end
+      before(:each) do
+        allow(client).to receive(:get).and_return(xml)
+      end
+
+      it "builds comment objects" do
+        expect(comments.length).to eq 30
+      end
+
+    end
+
+    context "bad URL" do
+      
+      let(:client) { Wordpress::Comments::Client.new 'not a URL' }
+
+      it "raises error" do
+        expect {
+          client.fetch
+        }.to raise_error(Errno::ENOENT)
+      end
+
+    end
+
+    context "bad XML" do
+      
+      before(:each) do
+      	allow(client).to receive(:get).and_return("Bad XML!")
+      end
+      
+      it "raise error from Nokogiri" do
+        expect {
+          client.fetch
+        }.to raise_error(Nokogiri::XML::SyntaxError)
+      end
+      
+    end
+
   end
 end
